7c161abc78aa71abad63c42d65ed6cf0ecec070f
Pragati-3003 <jpragati3003@gmail.com>
2025-01-01 15:40:04 +0530

    Student Attendance report added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,15 @@ import './App.css'
 import './index.css'
 import Assignment from './Components/Assignment/Assignment'
 import TimeTable from './Components/TimeTable/TimeTable'
+import Attendance from './Components/Attendance_student/Attendance'
 function App() {
 
   return (
-    <>
-     <Assignment/>
+    <div className='h-auto '>
+     {/* <Assignment/> */}
      {/* <TimeTable/> */}
-     
-    </>
+     <Attendance />
+    </div>
   )
 }
 
