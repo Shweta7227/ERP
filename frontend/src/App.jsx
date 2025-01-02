84f53e768652fab3f17eb4ed0342fa9d31044b01
Shweta7227 <sindhushweta02@gmail.com>
2025-01-03 00:23:52 +0530

    fees page is added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,15 @@ import './index.css'
 import Assignment from './Components/Assignment/Assignment'
 import TimeTable from './Components/TimeTable/TimeTable'
 import Attendance from './Components/Attendance_student/Attendance'
+import FeeStructure from './Components/FeeStructure/FeeStructure'
 function App() {
 
   return (
     <div className='h-auto '>
      {/* <Assignment/> */}
      {/* <TimeTable/> */}
-     <Attendance />
+     <FeeStructure />
+
     </div>
   )
 }
